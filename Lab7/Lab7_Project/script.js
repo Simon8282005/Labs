c81ce03095c9bdaf61f700f616bdf2a743ff2746
Simon8282005 <simonjava2019@gmail.com>
2025-12-17 01:27:49 +0800

    Change Name

---
+++ Lab7/Lab7_Project/script.js
@@ -0,0 +1,44 @@
+let slideIndex = 0;
+
+// Showing the collapse content
+document.addEventListener("DOMContentLoaded", function() {
+    //const collapsibleButton = document.querySelector(".collapsible");  // can only select specific components
+    const collapsibleButton = document.querySelectorAll(".collapsible");
+    //const content = document.querySelectorAll(".answer");
+
+    collapsibleButton.forEach(function(button) {
+        button.addEventListener("click", function() {
+            const content = this.nextElementSibling;
+
+            if (content.style.display === "block") {
+                content.style.display = "none";
+            } else {
+                content.style.display = "block";
+            }
+        });
+    });
+});
+
+
+
+// Slide show function
+function showSlides() {
+    let slide = document.getElementsByClassName('slide');
+
+    for (let i = 0; i < slide.length; i++) {
+        slide[i].style.display = "none";
+    }
+
+    slideIndex++;
+ 
+     if (slideIndex > slide.length) {
+        slideIndex = 1;
+     }
+
+     slide[slideIndex - 1].style.display = "block";
+
+     setTimeout(showSlides, 3000);
+}
+
+// Initialize the slideshow
+showSlides();
